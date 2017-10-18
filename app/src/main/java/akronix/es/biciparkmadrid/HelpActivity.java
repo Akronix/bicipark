9765d33d00e459a376ee5dac6deca0625fb6586d
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-10-18 14:01:04 +0200

    Added dial intent to support telephone

--- app/src/main/java/akronix/es/biciparkmadrid/HelpActivity.java
+++ app/src/main/java/akronix/es/biciparkmadrid/HelpActivity.java
@@ -1,13 +1,21 @@
 package akronix.es.biciparkmadrid;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class HelpActivity extends AppCompatActivity {
+
+    String phone = "+34915298210"; // Teléfono para Atención al ciudadano de Madrid
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,16 @@ public class HelpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void callForHelp(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:" + phone));
+        try {
+            startActivity(callIntent);
+        } catch (Exception e) {
+            Log.e(MainActivity.LOG_TAG, "Failed to invoke call", e);
+        }
     }
 
 }
