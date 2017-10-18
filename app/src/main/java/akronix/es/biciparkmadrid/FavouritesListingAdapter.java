c98bd5b4f7276f8a2846990636c4e18d79a90a64
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-10-18 19:05:46 +0200

    Show context actions pop-up for favourite parkings

--- app/src/main/java/akronix/es/biciparkmadrid/FavouritesListingAdapter.java
+++ app/src/main/java/akronix/es/biciparkmadrid/FavouritesListingAdapter.java
@@ -1,14 +1,8 @@
 package akronix.es.biciparkmadrid;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
-import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.widget.CursorAdapter;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,75 +16,39 @@ public final class FavouritesListingAdapter extends CursorAdapter {
 
     private LayoutInflater mInflater;
     private static int layout = R.layout.favourite_record;
-    private static String[] from = { "name" };
+    private static String[] from = {
+            DBHelper.FAVOURITES_COLUMN_NAMES.parking_id.toString(),
+            DBHelper.FAVOURITES_COLUMN_NAMES.name.toString() };
     private Context mContext;
+    private ListFavouritesActivity mActivity;
 
 
-    public FavouritesListingAdapter(Context context, Cursor c) {
-        super(context, c, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-        this.mContext = context;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public FavouritesListingAdapter(ListFavouritesActivity activity, Cursor c) {
+        super(activity, c, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        this.mActivity = activity;
+        this.mContext = activity;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, final ViewGroup parent) {
         View view = mInflater.inflate(layout, parent, false);
-        view.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                DialogFragment dialog = ContextActionDialog.newInstance(
-                        23);
-                dialog.show(parent.getFragmentManager(), "dialog");
-                return true;
-            }
-        });
         return view;
-    }
-
-    public static class ContextActionDialog extends DialogFragment {
-        long favouriteId;
-
-        static ContextActionDialog newInstance(int favouriteId) {
-            ContextActionDialog dialog = new ContextActionDialog();
-
-            // Supply num input as an argument.
-            Bundle args = new Bundle();
-            args.putInt("favouriteId", favouriteId);
-            dialog.setArguments(args);
-
-            return dialog;
-        }
-
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            final String [] contextActions = {
-                    getResources().getString(R.string.rename_favourite),
-                    getResources().getString(R.string.delete_favourite)
-            };
-            builder.setTitle(R.string.dialog_select_action)
-                    .setItems(R.array.favourites_context_actions, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            switch (which) {
-                                case 0:
-                                    getActivity().renameFavourite(favouriteId);
-                                    break;
-                                case 1:
-                                    getActivity().deleteFavourite(favouriteId);
-                                    break;
-                            }
-                        }
-                    });
-            return builder.create();
-        }
     }
 
 
     @Override
     public void bindView(View view, final Context context, Cursor cursor) {
         TextView tvName = (TextView) view.findViewById(R.id.tvName);
-        tvName.setText(cursor.getString(cursor.getColumnIndex(from[0])));
+        tvName.setText(cursor.getString(cursor.getColumnIndex(from[1])));
+        final long parkingId = cursor.getLong(cursor.getColumnIndex(from[0]));
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mActivity.showContextActionDialog(parkingId);
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/akronix/es/biciparkmadrid/ListFavouritesActivity.java
+++ app/src/main/java/akronix/es/biciparkmadrid/ListFavouritesActivity.java
@@ -1,17 +1,22 @@
 package akronix.es.biciparkmadrid;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,5 +47,37 @@ public class ListFavouritesActivity extends AppCompatActivity {
             );
 
         listView.setAdapter(cursorAdapter);
+    }
+
+    public void showContextActionDialog(long id) {
+        final long favouriteId = id;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final String [] contextActions = {
+                getResources().getString(R.string.rename_favourite),
+                getResources().getString(R.string.delete_favourite)
+        };
+        builder.setTitle(R.string.dialog_select_action)
+                .setItems(R.array.favourites_context_actions, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                renameFavourite(favouriteId);
+                                break;
+                            case 1:
+                                deleteFavourite(favouriteId);
+                                break;
+                        }
+                    }
+                });
+        builder.create().show();
+    }
+
+    public void renameFavourite(long favouriteId) {
+        Toast.makeText(this, "Renaming " + favouriteId, Toast.LENGTH_SHORT).show();
+    }
+
+    public void deleteFavourite(long favouriteId) {
+        Toast.makeText(this, "Deleting " + favouriteId, Toast.LENGTH_SHORT).show();
     }
 }
