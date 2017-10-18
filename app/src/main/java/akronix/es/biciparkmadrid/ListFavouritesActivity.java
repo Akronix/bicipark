8ccd51fd894a08e5cae027b81eabaa624cbb4757
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-10-18 13:45:58 +0200

    List favorites activity changed to a basic child activity

--- app/src/main/java/akronix/es/biciparkmadrid/ListFavouritesActivity.java
+++ app/src/main/java/akronix/es/biciparkmadrid/ListFavouritesActivity.java
@@ -1,15 +1,23 @@
 package akronix.es.biciparkmadrid;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ListFavouritesActivity extends AppCompatActivity {
+public class ListFavouritesActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
     private DBAdapter mDBAdapter;
     @BindView(R.id.lvFavourites) ListView listView;
@@ -19,6 +27,10 @@ public class ListFavouritesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_favourites);
         ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mDBAdapter = new DBAdapter(this);
         populateList();
@@ -35,6 +47,31 @@ public class ListFavouritesActivity extends AppCompatActivity {
         );
 
         listView.setAdapter(cursorAdapter);
+    }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_map) {
+            // Already here, do nothing
+        } else if (id == R.id.nav_favourites) {
+            Intent intent = new Intent(this, ListFavouritesActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
